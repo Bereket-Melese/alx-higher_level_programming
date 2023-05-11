9fd62248118c5f07304dfbe11bdedbc650003863
Bereket-Melese <123330690+Bereket-Melese@users.noreply.github.com>
2023-05-11 23:48:44 +0300

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python3
-from sys import argv
-from calculator_1 import add, sub, mul, div
-
-
 if __name__ == "__main__":
-    argc = len(argv) - 1
-    if argc != 3:
+    import sys
+    from calculator_1 import add, sub, mul, div
+
+    if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
-    operator = argv[2]
-    a = int(argv[1])
-    b = int(argv[3])
+        sys.exit(1)
+
+    a = int(sys.argv[1])
+    operator = sys.argv[2]
+    b = int(sys.argv[3])
+
     if operator == "+":
-        print("{} + {} = {}".format(a, b, add(a, b)))
+        result = add(a, b)
     elif operator == "-":
-        print("{} - {} = {}".format(a, b, sub(a, b)))
+        result = sub(a, b)
     elif operator == "*":
-        print("{} * {} = {}".format(a, b, mul(a, b)))
+        result = mul(a, b)
     elif operator == "/":
-        print("{} / {} = {}".format(a, b, div(a, b)))
+        result = div(a, b)
     else:
         print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
+        sys.exit(1)
+
+    print("{} {} {} = {}".format(a, operator, b, result))
