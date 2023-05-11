a58a544cb0b3aaa62450e5d24533485e2a4b804d
Bereket-Melese <123330690+Bereket-Melese@users.noreply.github.com>
2023-05-11 23:41:59 +0300

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-import hidden_4 as h
 
 if __name__ == "__main__":
-    names = dir(h)
-    i = 0
+    """Print all names defined by hidden_4 module."""
+    import hidden_4
 
-while i < len(names):
-    if names[i][:2] != '__':
-        print(names[i])
-    i += 1
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
